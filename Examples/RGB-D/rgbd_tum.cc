ac3524ccac49e8e8a3133ac13337b2eaadaa858e
Handuo <zhanghanduo@live.cn>
2019-02-07 10:24:54 +0800

    :pencil: fix small load arg num bug

--- Examples/RGB-D/rgbd_tum.cc
+++ Examples/RGB-D/rgbd_tum.cc
@@ -35,7 +35,7 @@ void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageF
 
 int main(int argc, char **argv)
 {
-    if(argc != 5)
+    if(argc != 4)
     {
         cerr << endl << "Usage: ./rgbd_tum path_to_settings path_to_sequence path_to_association" << endl;
         return 1;
